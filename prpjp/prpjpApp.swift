0773d9a40d39b0a9058b56d5acff3df31c316f6f
createvaluefor <2019143077@yonsei.ac.kr>
2022-05-31 21:49:59 +0900

    Initial Commit

---
+++ prpjp/prpjpApp.swift
@@ -0,0 +1,17 @@
+//
+//  prpjpApp.swift
+//  prpjp
+//
+//  Created by 이민기 on 2022/05/31.
+//
+
+import SwiftUI
+
+@main
+struct prpjpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
